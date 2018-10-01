d4e5a5568b06d8aeac4d7704ab5b4533419b4cc3
VitalyR <vitalyankh@gmail.com>
2018-10-01 18:50:09 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! #Algorithms
+//! A library for most common algorithms.
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+pub mod search;
+
+pub mod sort;
+
