298a7bb10c462a762815db4a5f3f03e659604627
VitalyR <vitalyankh@gmail.com>
2019-03-11 22:21:24 +0800

    sorting algorithms

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,44 @@
 //! #Algorithms
 //! A library for most common algorithms.
 
+pub mod searching;
+pub mod sorting;
+pub mod data;
+
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn quick_sort() {
+        //descending
+        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
+        crate::sorting::quick_sort(&mut ve1);
+        for i in 0..ve1.len() - 1 {
+            assert!(ve1[i] <= ve1[i + 1]);
+        }
+
+        //pre-sorted
+        let mut ve2 = vec![1, 2, 3, 4, 5, 6];
+        crate::sorting::quick_sort(&mut ve2);
+        for i in 0..ve2.len() - 1 {
+            assert!(ve2[i] <= ve2[i + 1]);
+        }
+    }
+    #[test]
+    fn bubble_sort() {
+        //descending
+        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
+        crate::sorting::bubble_sort(&mut ve1);
+        for i in 0..ve1.len() - 1 {
+            assert!(ve1[i] <= ve1[i + 1]);
+        }
+
+        //pre-sorted
+        let mut ve2 = vec![1, 2, 3, 4, 5, 6];
+        crate::sorting::bubble_sort(&mut ve2);
+        for i in 0..ve2.len() - 1 {
+            assert!(ve2[i] <= ve2[i + 1]);
+        }
     }
 }
-pub mod search;
-
-pub mod sorting;
 
---
+++ src/sorting.rs
@@ -0,0 +1,46 @@
+mod bubble_sort;
+mod quick_sort;
+mod selection_sort;
+
+use std::cmp;
+
+pub use self::bubble_sort::bubble_sort;
+pub use self::quick_sort::quick_sort;
+pub use self::selection_sort::selection_sort;
+
+pub fn is_sorted<T>(arr: &[T]) -> bool
+    where
+        T: cmp::PartialOrd,
+{
+    if arr.is_empty() {
+        return true;
+    }
+
+    let mut prev = &arr[0];
+
+    for item in arr.iter().skip(1) {
+        if prev > &item {
+            return false;
+        }
+
+        prev = &item;
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn is_sorted() {
+        use super::*;
+
+        assert!(is_sorted(&[] as &[isize]));
+        assert!(is_sorted(&["a"]));
+        assert!(is_sorted(&[1, 2, 3]));
+        assert!(is_sorted(&[0, 1, 1]));
+
+        assert_eq!(is_sorted(&[1, 0]), false);
+        assert_eq!(is_sorted(&[2, 3, 1, -1, 5]), false);
+    }
+}
